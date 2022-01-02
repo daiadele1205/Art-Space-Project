86783e4c7d442eaecac9379a10e6bcec0fdca1dd
Ryan Montelibano <ryanjosephmonty@gmail.com>
2022-01-02 14:52:51 -0500

    Reworking Front page

---
+++ Migrations/20220102192356_addImageToArtworkType.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Art.Migrations
+{
+    public partial class addImageToArtworkType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "ArtworkType",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "ArtworkType");
+        }
+    }
+}
---
+++ Migrations/20220102194211_deleteImageColumnFromArtworkType.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Art.Migrations
+{
+    public partial class deleteImageColumnFromArtworkType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "ArtworkType");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "ArtworkType",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
