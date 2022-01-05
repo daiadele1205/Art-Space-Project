b69de41d8709c0e96339cc459d4858b91b6557a2
Adele <91921081+daiadele1205@users.noreply.github.com>
2022-01-04 22:46:36 -0500

    Fixed User Lock Button

--- Areas/Admin/Controllers/UserController.cs
+++ Areas/Admin/Controllers/UserController.cs
@@ -28,5 +28,46 @@ namespace Art.Areas.Admin
 
             return View(await _db.ApplicationUser.Where(u => u.Id != claim.Value).ToListAsync());
         }
+        public async Task<IActionResult> Lock(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var applicationUser = await _db.ApplicationUser.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (applicationUser == null)
+            {
+                return NotFound();
+            }
+
+            applicationUser.LockoutEnd = DateTime.Now.AddYears(1000);
+
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> UnLock(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var applicationUser = await _db.ApplicationUser.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (applicationUser == null)
+            {
+                return NotFound();
+            }
+
+            applicationUser.LockoutEnd = DateTime.Now;
+
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
